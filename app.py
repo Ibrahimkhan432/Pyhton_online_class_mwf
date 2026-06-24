eb10ff93a1256916fd002af163515091e99a4214
ibrahimkhan432 <ibrahimsheikh772@gmail.com>
2026-06-24 17:19:57 +0500

    functions

---
+++ app.py
@@ -0,0 +1,47 @@
+# print("hello")
+# functions
+# code - controlled
+# maintain
+# reusable
+
+# naming convention
+#  myFunction
+# -myfunction
+#  4function
+# _my_function
+
+# def myFunction():
+#  print("hello this is funtion")
+# myFunction()  #invoke  
+
+# def myname(fname,lname):  # parameter
+#   print("my name is ",  fname , lname)
+# myname("ibrahim" , "khan")  # argument
+
+# def sum(num1,num2):
+#    print(num1 + num2)
+# sum(2,3)   
+  
+# def sum(num1,num2=0): #default parameters
+#    print(num1 + num2)
+# sum(2)   
+
+# def sum(a,b):
+#    return a + b 
+   
+# newvariable = sum(2,3)
+# print(newvariable)
+
+# def yourname(name="monster"):
+#      print(name)
+# yourname()    
+# yourname("danish")    
+# yourname("khan")    
+
+# def sum(a,b):
+#     print("sum=>",a+b)
+# sum(a=2,b=3) 
+
+def fname(fname,lname):
+    print("my fname is", fname + " " + " " +  "and my lname is",lname)
+fname("ibrahim","khan")
