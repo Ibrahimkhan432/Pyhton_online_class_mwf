7c64d9b67f0b31b8d1a59f00a0fe31a4347b05bc
ibrahimkhan432 <ibrahimsheikh772@gmail.com>
2026-06-29 17:28:26 +0500

    data and interactivity

--- app.py
+++ app.py
@@ -3,19 +3,20 @@
 # code - controlled
 # maintain
 # reusable
+# readable
 
 # naming convention
 #  myFunction
+# _my_function
 # -myfunction
 #  4function
-# _my_function
 
 # def myFunction():
 #  print("hello this is funtion")
 # myFunction()  #invoke  
 
 # def myname(fname,lname):  # parameter
-#   print("my name is ",  fname , lname)
+#   print("my name is" + " jnew " , fname,lname)
 # myname("ibrahim" , "khan")  # argument
 
 # def sum(num1,num2):
@@ -42,6 +43,50 @@
 #     print("sum=>",a+b)
 # sum(a=2,b=3) 
 
-def fname(fname,lname):
-    print("my fname is", fname + " " + " " +  "and my lname is",lname)
-fname("ibrahim","khan")
+# def fname(fname,lname,fullname):
+#     print("my fname is", fname,fullname + "  " +  " " +  "and my lname is",lname)
+# fname("ibrahim","khan" , "ibrahim khan")
+
+# def sum():
+#  return ("hello")
+# againsum = sum()
+# print(againsum)
+
+# a = 2 # assign
+# a == 2 # equal to 
+# "2" === 2 # equal with type
+
+# -- topic (data and interactivity)
+# data - information
+# which use for input and output process
+   # -data types
+# name = "ibrahim" - string
+# age = 15 - integer (int)
+# height = 5.9 - float
+# isActive = true - boolean (bool)
+
+# sum = 2 + "3" # error
+# sum = "2" + "3" #concatenation
+# sum = 2 + 3.5 # temporary (coercion)
+# total = 300
+# obtain = "100"
+# percent = obtain / total * 100
+# print(type(percent))
+# print(type(obtain))
+
+# --- interactivity
+# input()
+# num1 = 3
+# num2 = 4
+# sum = num1 + num2
+# print(sum)
+
+# num1 = +input("enter first num ")
+# num2 = +input("enter second num ")
+# # sum = int(num1) + int(num2)
+# sum = num1 + num2
+# print(sum)
+
+obtain = int(input("enter your marks : "))
+total = int(input("enter total marks : "))
+print("your percentage is ", obtain / total *100)
