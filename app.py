e834959c48217e9fdb3be48e0b2d55c7e4459825
ibrahimkhan432 <ibrahimsheikh772@gmail.com>
2026-07-01 17:27:58 +0500

    list with assignment

--- app.py
+++ app.py
@@ -91,6 +91,69 @@
 # total = int(input("enter total marks : "))
 # print("your percentage is ", obtain / total *100)
 
+# asssignment
+# .function(fullname)
+# .input (user multiplication)
+
+
+#  --- list
+std1 = "ali"
+std2 = "bilal"
+std3 = "ayan"
+std4 = "asad"
+
+# stduntlist = ["ali","bilal","ayan","asad"]
+# print(stduntlist)
+# # list - rules
+#  [ ] , [ , ] - comma seperated
+# stduntlist = ["ali",20,76.76,True]
+# order         1    2   3    4 (length) -total elements
+# order access. 0    1   2    3 (index)start from zero
+# print(len(stduntlist)) - length
+# print(stduntlist[0])
+# print(stduntlist[1])
+# print(stduntlist[4]) - out of range error
+# print(stduntlist[-1])
+# print(stduntlist[-2])
+# print(stduntlist[2:])
+# index order cannot be changed
+# list can be modify (changeable)
+
+
+# mydata = ["ali",20,"ali@gmail.com"]
+# mydata.append(30) - add in last
+# mydata.insert(2,"danish") #- 2 is index , "danish" is element
+# mydata.remove("ali@gmail.com")
+# mydata.remove("khan") - not exist error
+# mydata.pop(0)
+# del mydata[2] # del is keyword
+# mydata.clear()
+# print(mydata)
+
+# str, int,list, (reserve) not use in variable name
+# list1 = ["a","b","c"]
+# list2 = ["d","e","f"]
+# list1.extend(list2) # method (formula)
+# list1.remove() #formula
+# list1.pop()
+# print(list1)
+
+# a = "b"
+# list2 = [a , "a"]
+# print(list2)
+# "a" - string , a -variable
+
+# list1 = ["a","b","c"]
+# # list1.pop()
+# list1.pop(1)
+# print(list1)
+
+list1 = ["a","b","c"]
+# list1[0] = "khan"
+list1[0:2] = ["ibrahim","khan"]
+print(list1)
+
 asssignment
-.function(fullname)
-.input (user multiplication)
+create a list in which you can connect 2 lists
+create a list and remove second index with 2 new add values
+create a list and remove first and last element using method
